3f4ca5d33d130856624cd07ec2b6727c29aa9f6c
xzmzm <xingzhimeng@gmail.com>
2024-02-28 16:37:31 +0800

    fixed endless mode

--- xzmzm-RestartWave/extensions/main.gd
+++ xzmzm-RestartWave/extensions/main.gd
@@ -19,20 +19,21 @@ func _ready():
 	_restart_wave_menu.connect("restart_wave_cancel_by_input", self, "_on_restart_wave_cancel_by_input")
 	
 func _on_player_died(_p_player:Player)->void :
+	if RunData.current_wave > 1:
+		pause_before_choose_restart_wave()
+		ModLoaderLog.info("player is killed", "RestartWave")
+		if not _restart_wave_menu.visible:
+			_restart_wave_menu.show()
+			_restart_wave_menu.reset()
+			_restart_wave_timer.start(10)
+		return
+	end_run()
+
+func end_run():
+	_player_life_bar.hide()
 	if RunData.current_wave <= RunData.nb_of_waves:
-		if RunData.current_wave > 1:
-			pause_before_choose_restart_wave()
-			ModLoaderLog.info("player is killed", "RestartWave")
-			if not _restart_wave_menu.visible:
-				_restart_wave_menu.show()
-				_restart_wave_menu.reset()
-				_restart_wave_timer.start(10)
-			return
-		else:
-			_player_life_bar.hide()
-			_is_run_lost = true
-	else:
-		_player_life_bar.hide()
+		_is_run_lost = true
+	else: # endless run
 		_is_run_won = true
 	clean_up_room(false, _is_run_lost, _is_run_won)
 	_end_wave_timer.start()
@@ -53,12 +54,7 @@ func _on_cancel_restart_wave():
 		_restart_wave_menu.hide()
 	ModLoaderLog.info("cancel restart wave", "RestartWave")
 	unpause_after_choose_restart_wave()
-	_player_life_bar.hide()
-	_is_run_lost = true
-	clean_up_room(false, _is_run_lost, _is_run_won)
-	_end_wave_timer.start()
-	ProgressData.reset_run_state()
-	ChallengeService.complete_challenge("chal_rookie")
+	end_run()
 	
 func _on_restart_wave_cancel_by_input():
 	ModLoaderLog.info("restart wave cancel by input", "RestartWave")
